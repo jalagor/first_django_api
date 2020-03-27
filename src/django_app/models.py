7ab9caa43bd24c245254fa5c9a3b79d7e75123b3
jalagor <johnrossrogla@gmail.com>
2020-03-26 23:36:05 -0600

    readme

--- src/django_app/models.py
+++ src/django_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. there si stuff here too
 class Bear(models.Model):
     name = models.CharField(max_length=20)
     age = models.IntegerField()
